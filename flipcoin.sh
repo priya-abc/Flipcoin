9f4b06ae511254a05645661817eb5cb66ba4edb0
priya-abc <priyapriya6598@gmail.com>
2020-11-04 17:57:29 +0530

    Added: Flip coin simulation to display winner is added

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 #Welcome message
 echo "Flip coin simulation winnings of heads and tails are done in this program"
+flip=$((RANDOM%2))
+h=0
+t=0
+diff=0
+flag=0
+while (( $diff < 2 ))
+do
+flip=$((RANDOM%2))
+   if (( $flip == 1 ))
+   then
+   ((h++))
+   if(( $t == 21 ))
+   then
+       continue
+fi
+else
